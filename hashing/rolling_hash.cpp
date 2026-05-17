d388336af6b44493372a8635e0e05cd3017d3325
Farhan <chowdhury.farhan235@gmail.com>
2026-05-17 17:18:31 +1000

    update rolling hash function to stop using int casts

--- hashing/rolling_hash.cpp
+++ hashing/rolling_hash.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ull = unsigned long long;
 
 // Return a list of the rolling hash of every subarray of size k in nums
 // Assumes that nums[i] is non-negative.
-vector<int> rollingHash(vector<int> nums, int k) {
-    using ull = unsigned long long;
+vector<ull> rollingHash(vector<int> nums, int k) {
     ull BASE = 1e6+3; // update this so that it is a prime number roughly equal to the number of unique characters in input array
     ull P = (1ULL<<31) - 1;
-    vector<int> res;
+    vector<ull> res;
     ull pow = 1;
     for (int i = 1; i < k; ++i) pow = (pow * BASE) % P; 
     ull hash = 0;
     for (int i = 0; i < k; ++i) hash = (hash * BASE + ((ull)nums[i] + P)) % P; 
-    res.push_back((int)hash); 
+    res.push_back(hash); 
     for (int i = k; i < nums.size(); ++i) { 
         ull remove = (((ull)nums[i - k] + P) * pow) % P;
         hash = (hash + P - remove) % P;
         hash = (hash * BASE) % P;
         hash = (hash + (ull)nums[i] + P) % P;
-        res.push_back((int)hash);
+        res.push_back(hash);
     }
     return res;
 }
@@ -26,7 +26,7 @@ vector<int> rollingHash(vector<int> nums, int k) {
 void Solve(){
     // use case:
     vector<int> nums({34, 354, 12, 76, 12324, 12 , 3546});
-    vector<int> hash3 = rollingHash(nums, 3);
+    auto hash3 = rollingHash(nums, 3);
     for (const auto& hash : hash3) cout << hash << endl;
 }
 
